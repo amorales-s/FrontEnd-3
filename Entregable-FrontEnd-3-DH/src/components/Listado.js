2c228b69317cf57c31b4ec44746eeb899c3aa77d
Alexander Morales <casanare@gmail.com>
2022-08-30 23:05:53 -0500

    Primer Entregable FrontEnd-3

---
+++ Entregable-FrontEnd-3-DH/src/components/Listado.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import VariosItem from './Item';
+import datos from './data.json';
+// Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
+// El componente Listado es el padre de:
+// - varios Item
+// ESTADO: Listado no necesita manejar un estado.
+// MÉTODOS: Listado no requiere de métodos.
+// PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
+
+export default function Listado({aumentar}) {
+
+  return (
+      <div className='container'>
+        {datos.map((item) =>{
+            return <VariosItem producto={item.producto}  key={item.id} stock={item.stock} aumentar= {aumentar}/>;
+        })
+        }
+      {/* renderizamos los Item aquí */}
+    </div>
+  );
+}
